3ab375fa0ab9f28ea3886f08b3162d5485095746
심청이 대표이사 <85424081+sookyoungwoo@users.noreply.github.com>
2023-04-13 12:08:14 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,30 +1,30 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "woosookyoung",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
     email: "morethanmin.dev@gmail.com",
     linkedin: "morethanmin",
-    github: "morethanmin",
+    github: "sookyoungwoo",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
+      name: `개발새발`,
       href: "https://github.com/morethanmin/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
-    theme: "light", // ['light', 'dark', 'auto']
+    title: "우수경",
+    description: "우수경임니당",
+    theme: "auto", // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://woosoolog.vercel.app/",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
@@ -34,9 +34,8 @@ const CONFIG = {
 
   // notion configuration (required)
   notionConfig: {
-    pageId:"a4e09b0963da4a50b90ed6e7ff584421",
+    pageId: process.env.NOTION_PAGE_ID,
   },
-
   // plugin configuration (optional)
   googleAnalytics: {
     enable: false,
@@ -56,13 +55,6 @@ const CONFIG = {
       repo: "morethanmin/morethan-log",
       "issue-term": "og:title",
       label: "💬 Utterances",
-    },
-  },
-  cusdis: {
-    enable: false,
-    config: {
-      host: "https://cusdis.com",
-      appid: "", // Embed Code -> data-app-id value
     },
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
